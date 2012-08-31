605a197169db8bac043b7f692f1b86dbb215ae68
Matej Cotman <matej.cotman@gmx.com>
2012-08-31 23:08:59 +0200

    added my own implementation of notify window

--- mynotify.py
+++ mynotify.py
@@ -1,64 +1,120 @@
-from Tkinter import *
-import string
-
 # on lost focus activate timer, on gain kill timer
 # or add to previous window
+# choose color
+
+import Tkinter
+import string
+import threading
+import time
 
 
-def notifysend(title, data):
+class Notification:
 
-    foreground = "white"
-    background = "#000022"
-    border_color = "blue"
+    N_BLUE = ("white", "#000022", "blue")
+    N_GREEN = ("white", "#002200", "green")
 
-    root = Tk()
-    root.withdraw()
 
-    def click(arg):
-        root.destroy()
+    class LifeThread(threading.Thread):
+        def __init__(self, parent, title, data, colors):
+            threading.Thread.__init__(self)
+            self.end = False
+            self.parent = parent
+            self.title = title
+            self.data = data
+            self.colors = colors
+            self.daemon = True
+            self.start()
 
-    window = Toplevel(bg=background, relief=FLAT, highlightcolor=border_color, highlightbackground=border_color, highlightthickness=1)
-    window.overrideredirect(1)
-    window.geometry("-10+10")
-    window.bind("<Button-1>", click)
+        def __del__(self):
+            self.end = True
 
-    dataarray = data.split("\n")
-    dataarray = [s for s in dataarray if string.strip(s)]
-    data_width = max([len(s) for s in dataarray])
-    data_height = len(dataarray)
+        def run(self):
+            self._notify()
 
-    label = Label(
-        window,
-        text=title,
-        bg=background,
-        fg=foreground,
-        relief=FLAT,
-        borderwidth=5
-    )
-    label.config(font=(None, 12, 'bold'))
-    label.bind("<Button-1>", click)
-    label.pack()
+        def _notify(self):
+            foreground = self.colors[0]
+            background = self.colors[1]
+            border_color = self.colors[2]
 
-    text = Text(
-        window,
-        width=data_width,
-        height=data_height,
-        cursor="arrow",
-        bg=background,
-        fg=foreground,
-        relief=FLAT,
-        borderwidth=5
-    )
-    text.config(font=(None, 10))
-    text.bind("<Button-1>", click)
-    text.insert(INSERT, "\n".join(dataarray))
-    text.config(state=DISABLED)
-    text.config(highlightbackground=background)
-    text.pack()
+            self.root = Tkinter.Tk()
+            self.root.withdraw()
 
-    #root.geometry("{0}x{1}-10+10".format(500, 300))
-    #root.geometry("{0}x{1}-10+10".format(500, 300))
-    #root.wm_attributes("-topmost", 1)
-    root.mainloop()
+            def click(arg):
+                self.window.withdraw()
 
-notifysend("WeeChat", "nek text nek\n        \n\ntext\n nek text\nloooooooooooooooooooooooooooooooooooooong teeeeeeeeeeext")
+            self.window = Tkinter.Toplevel(
+                relief=Tkinter.FLAT,
+                highlightthickness=1
+            )
+            self.window.overrideredirect(1)
+            self.window.geometry("-10+10")
+            self.window.bind("<Button-1>", click)
+
+            self.label = Tkinter.Label(
+                self.window,
+                relief=Tkinter.FLAT,
+                borderwidth=5
+            )
+            self.label.config(font=(None, 11, 'bold'))
+            self.label.bind("<Button-1>", click)
+            self.label.pack()
+
+            self.text = Tkinter.Text(
+                self.window,
+                cursor="arrow",
+                relief=Tkinter.FLAT,
+                borderwidth=5,
+                highlightthickness=0,
+                highlightbackground=background
+            )
+            self.text.config(font=(None, 9))
+            self.text.bind("<Button-1>", click)
+            self.text.pack()
+
+            self.change_notification(self.title, self.data, self.colors)
+
+            self.root.mainloop()
+            self.end = True
+
+        def change_notification(self, title, data, colors):
+            self.title = title
+            self.data = data
+            self.colors = colors
+
+            self.label['text'] = self.title
+            self.label['fg'] = self.colors[0]
+            self.label['bg'] = self.colors[1]
+
+            self.text['fg'] = self.colors[0]
+            self.text['bg'] = self.colors[1]
+            dataarray = self.data.split("\n")
+            dataarray = [s for s in dataarray if string.strip(s)]
+            data_width = max([len(s) for s in dataarray])
+            data_height = len(dataarray)
+            self.text.config(state=Tkinter.NORMAL)
+            self.text.delete(1.0, Tkinter.END)
+            self.text.insert(Tkinter.END, "\n".join(dataarray))
+            self.text.config(state=Tkinter.DISABLED)
+            self.text['width'] = data_width
+            self.text['height'] = data_height
+
+            self.window['bg'] = self.colors[1]
+            self.window['highlightbackground'] = self.colors[2]
+            self.window['highlightcolor'] = self.colors[2]
+
+            self.window.deiconify()
+
+    def is_alive(self):
+        return hasattr(self, "life") and self.life and not self.life.end
+
+    def notify(self, title, data, colors):
+        if self.is_alive():
+            self.life.change_notification(title, data, colors)
+        else:
+            self.life = Notification.LifeThread(self, title, data, colors)
+
+    def show(self):
+        self.life.window.deiconify()
+
+    def hide(self):
+        self.life.window.withdraw()
